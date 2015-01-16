1f542377775b86f4a3fc9bf828a99309c8aad0a3
Dorian <dorian.lenzner@mailbox.tu-dresden.de>
2015-01-16 20:12:51 +0100

    Kommentare eingefügt

--- fakultaet.c
+++ fakultaet.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> //Standardbibliothek für Ein- und Ausgabe
 
-int fakultaet(int a) {
+int fakultaet(int a) { //Funktion zum berechnen der Fakultät
 	if (a == 0)
 	{
-		return 0;
+		return 1;
 	}
 	else {
-		return (a * fakultaet(a-1));
+		return (a * fakultaet(a-1)); //Die Funktion ruft sich rekursiv selbst auf, bis die Abbruchbedingung erreicht ist
 	}
 }
 
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
 {
 	int eingabe;
 	printf("Bitte eine Zahl eingeben\n");
-	scanf("%d", &eingabe);
+	scanf("%d", &eingabe); //Der Wert der Eingabe wird auf der Variable eingabe abgelegt
 	printf("%d\n", fakultaet(eingabe));
 	return 0;
 }
