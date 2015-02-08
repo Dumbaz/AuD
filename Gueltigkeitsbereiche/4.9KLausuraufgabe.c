8870e63c3506eca047b06cefaca6f894e484328d
Dumbaz <iwantmyfingerback@gmail.com>
2015-02-08 03:23:22 +0100

    4.9 Klausuraufgabe mit Loesung fuer a) und b)

--- Gueltigkeitsbereiche/4.9KLausuraufgabe.c
+++ Gueltigkeitsbereiche/4.9KLausuraufgabe.c
@@ -47,3 +47,40 @@ int e:  24-29
 int a:  24-29
 
 */
+
+/* Pulsierender Speicher
+   Haltepunkt   RM          Umgebung
+                            1   2   3   4   5   6   7   8   9
+
+   label 5      -           e   a
+                            2   ?
+
+   label 3      3                   a   b  
+                            2   1   #1  #2
+
+   label 1      2:3                         x   y   z
+                            2   1   #1  #2  #1  #2  1
+
+   label 3      1:2:3                                   a   b
+                            2   0   #1  #2  #1  #2  1   #1  #2     a=7 Laut Loesung, aber das erscheint mir falsch
+
+   label 2      2:3                         x   y   z
+                            2   0   #1  #2  #1  #2  1
+
+   label 4      3                   a   b
+                            2   0   #1  #2
+
+   label 1      2:3                          x   y   z
+                            2   0   #1  #2   #1  #2  0
+
+   label 2      2:3                         x   y   z
+                            0   0   #1  #2  #1  #2  0
+
+   label 4      3                   a   b
+                            0   0   #1  #2
+
+   label 6      -           e   a
+                            0   0
+
+   */
+   
