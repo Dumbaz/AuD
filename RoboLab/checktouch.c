d8fa33bbf0e8bcc7f30aca5a08108e2441b5e935
Dorian Lenzner <iwantmyfingerback@gmail.com>
2015-03-03 00:24:18 +0100

    Docstring
    One docstring

--- RoboLab/checktouch.c
+++ RoboLab/checktouch.c
@@ -1,11 +1,11 @@
 #include "../h/main.h"
 
 int checktouch(){ //Bei Berührung anhalten
-
+			//Both Sensors need to trigger
 	if (ecrobot_get_touch_sensor(NXT_PORT_S1) == 1 || ecrobot_get_touch_sensor(NXT_PORT_S4) == 1){
 
 			hardstop();
-
+							
 			ecrobot_sound_tone(1500,1000,50);
 
 			systick_wait_ms(1000*10); //10 Sekunden warten
